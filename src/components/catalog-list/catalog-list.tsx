5862e2e2a5c011f3a24d137f2f288dfb4d07f310
Ryu <privetslavka@yandex.com>
2024-01-24 14:49:14 +0300

    Добавляет правки в пагинацию

--- src/components/catalog-list/catalog-list.tsx
+++ src/components/catalog-list/catalog-list.tsx
@@ -4,12 +4,28 @@ import { usePaginate } from '../../hooks/usePaginate';
 import { selectCameras } from '../../store/cameras/cameras.selector';
 import { useAppSelector } from '../../hooks/useAppSelector';
 import { CARDS_PER_PAGE } from '../../const';
+import { useEffect } from 'react';
 
 export function CatalogList() {
   const cameras = useAppSelector(selectCameras);
   const paginatedData = usePaginate(cameras.length);
 
   const { firstIdx, lastIdx, currentPage, totalPage } = paginatedData;
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if (isMounted) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    }
+
+    return () => {
+      isMounted = false;
+    };
+  }, [currentPage]);
 
   return cameras.length === 0 ? (
     <h1>Нет доступных товаров.</h1>
